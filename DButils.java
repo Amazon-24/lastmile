3db2ddb50394f241e61483dd8bceb1022116ad77
shyamchow <shyammannuru98@gmail.com>
2024-03-17 15:02:37 +0530

    mergeconflict

--- DButils.java
+++ DButils.java
@@ -3,11 +3,15 @@ Hello Guys...
 This is java file which contains 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 This is new line iin master branch
 
 This line is updated from development branch...
 
+=======
+This line is updated from development branch...
+>>>>>>> 5f48eff (updated)
 =======
 This line is updated from development branch...
 >>>>>>> 5f48eff (updated)
